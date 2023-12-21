07d240cbadd8bef2da9c6b0ad063ed8ecc0c5db4
andromixa <andromiha2@gmail.com>
2023-12-21 17:22:37 +0500

    Updated test-function name

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from conftest import on_form
 
 
-def test_bucket(browser):
+def test_chart_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
 
